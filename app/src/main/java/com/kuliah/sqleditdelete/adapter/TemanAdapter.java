068232cbd19355197b50cf36f9304082e2feedb0
isajaisyullah <isa.jaisyullah@gmail.com>
2022-04-14 07:11:39 +0700

    Initial commit for activity 7 PAM

---
+++ app/src/main/java/com/kuliah/sqleditdelete/adapter/TemanAdapter.java
@@ -0,0 +1,109 @@
+package com.kuliah.sqleditdelete.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.kuliah.sqleditdelete.MainActivity;
+import com.kuliah.sqleditdelete.R;
+import com.kuliah.sqleditdelete.database.DBController;
+import com.kuliah.sqleditdelete.database.Teman;
+import com.kuliah.sqleditdelete.edit_teman;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
+    private ArrayList<Teman> listData;
+    private Context control;
+
+    public TemanAdapter(ArrayList<Teman> listData) {
+        this.listData = listData;
+    }
+
+    @Override
+    public TemanAdapter.TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutinf = LayoutInflater.from(parent.getContext());
+        View v = layoutinf.inflate(R.layout.row_data_teman, parent, false);
+        control = parent.getContext();
+        return new TemanViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TemanAdapter.TemanViewHolder holder, int position) {
+        String nma, tlp, id;
+
+        id = listData.get(position).getId();
+        nma = listData.get(position).getNama();
+        tlp = listData.get(position).getTelpon();
+        DBController db  = new DBController(control);
+
+        holder.namaTxt.setTextSize(30);
+        holder.namaTxt.setTextColor(Color.BLUE);
+
+        holder.namaTxt.setText(nma);
+        holder.telponTxt.setText(tlp);
+
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(control, holder.cardku);
+                popupMenu.inflate(R.menu.menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.mnEdit:
+                                Intent i = new Intent(control, edit_teman.class);
+                                i.putExtra("id", id);
+                                i.putExtra("nama", nma);
+                                i.putExtra("telpon", tlp);
+                                control.startActivity(i);
+                                break;
+                            case R.id.mnHapus:
+                                HashMap<String, String> values = new HashMap<>();
+                                values.put("id", id);
+                                db.DeleteData(values);
+                                Intent j = new Intent(control, MainActivity.class);
+                                control.startActivity(j);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return (listData != null)?listData.size() : 0;
+    }
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardku;
+        private TextView namaTxt, telponTxt;
+
+        public TemanViewHolder(View view) {
+            super(view);
+
+            cardku = (CardView) view.findViewById(R.id.kartuku);
+            namaTxt = (TextView) view.findViewById(R.id.textNama);
+            telponTxt = (TextView) view.findViewById(R.id.textTelpon);
+        }
+    }
+
+}
